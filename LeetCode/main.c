04597613dbd9cc57f8ed8c98ef9a2cbb4d40bb46
lidaojian <lidj@jiedaibao.com>
2018-01-11 14:00:30 +0800

    Add Test Code

--- LeetCode/main.c
+++ LeetCode/main.c
@@ -18,36 +18,31 @@ int main(int argc, const char * argv[]) {
     // insert code here...
    
     //  Two Sum
-    /**
-    int nums[4] = {1, 7, 8, 8};
-    int *result = twoSum(nums, 4, 9);
-    printf("%d %d\n", result[0], result[1]);
-    **/
+    int twoSum_nums[4] = {1, 7, 8, 8};
+    int *twoSum_result = twoSum(twoSum_nums, 4, 9);
+    printf("========= Two Sum ==========\n");
+    printf("the result is:\n%d %d\n", twoSum_result[0], twoSum_result[1]);
     
     //  Add Two Numbers
-    /**
-    int number1Array[3] = {5, 6, 7};
-    int number2Array[3] = {5, 6, 4};
-    struct ListNode *number1 = tailCreatList(number1Array, 3);
-    struct ListNode *number2 = tailCreatList(number2Array, 3);
-    printList(addTwoNumbers(number1, number2));
-     */
+    int addTwoNumbers_number1Array[3] = {5, 6, 7};
+    int addTwoNumbers_number2Array[3] = {5, 6, 4};
+    struct ListNode *addTwoNumbers_number1 = tailCreatList(addTwoNumbers_number1Array, 3);
+    struct ListNode *addTwoNumbers_number2 = tailCreatList(addTwoNumbers_number2Array, 3);
+    printf("========= Add Two Numbers ==========\n");
+    printList(addTwoNumbers(addTwoNumbers_number1, addTwoNumbers_number2));
     
     //  Reverse Integer
-    /**
+    printf("========= Reverse Integer ==========\n");
     printf("%d\n", reverse(1534236469));
     printf("%d\n", reverse(-123));
-    */
     
     //  Integer to Roman
-    /**
+    printf("========= Integer to Roman ==========\n");
     printf("%s\n", intToRoman(1456));
-     */
     
     //   Roman to Integer
-    /**
+    printf("========= Roman to Integer ==========\n");
     printf("%d\n", romanToInt("MCDLVI"));
-    */
     
     return 0;
 }
